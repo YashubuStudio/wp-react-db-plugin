60f19070809483c15bf0375bf6e46ec0d354b194
YashubuStudio <YashubuStudio@gmail.com>
2025-06-04 19:05:16 +0900

    Add listener to override /db hash redirect

--- react-db-plugin/includes/shortcode.php
+++ react-db-plugin/includes/shortcode.php
@@ -47,11 +47,6 @@ function reactdb_app_shortcode() {
         '1.0',
         true
     );
-    wp_add_inline_script(
-        'react-db-plugin-script',
-        "if (location.hash === '#/db') { location.hash = '#/'; }",
-        'after'
-    );
     wp_enqueue_style(
         'react-db-plugin-style',
         plugins_url('assets/app.css', dirname(__DIR__) . '/react-db-plugin.php'),
@@ -64,6 +59,11 @@ function reactdb_app_shortcode() {
         'currentUser' => $user->display_name,
         'logoutUrl'   => wp_logout_url()
     ]);
+    wp_add_inline_script(
+        'react-db-plugin-script',
+        "function reactdb_fix(){if(location.hash==='#/db'){location.hash='#/';}}window.addEventListener('hashchange',reactdb_fix);reactdb_fix();",
+        'after'
+    );
 
     return ob_get_clean();
 }
